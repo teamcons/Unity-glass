4ddcddfb512f5987de51597e3e506bbf2b11fee7
teamcons_atwork <littlepastelworld@gmail.com>
2024-05-27 17:38:16 +0200

    f

--- unityglass@teamcons.carrd.co/prefs.js
+++ unityglass@teamcons.carrd.co/prefs.js
@@ -5,23 +5,13 @@ import {ExtensionPreferences, gettext as _}
 from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 let Me;
 
-function newRow(settings, group, title, key) {
-    const actionRow = new Adw.ActionRow({
-        title: title,
-    });
-    group.add(actionRow);
-    const switcher = new Gtk.Switch({
-        active: settings.get_boolean(key),
-        valign: Gtk.Align.CENTER,
-    });
-    actionRow.add_suffix(switcher);
-    actionRow.set_activatable_widget(switcher);
-    settings.bind(key, switcher, 'active', Gio.SettingsBindFlags.DEFAULT);
-}
 
+// Generale a row for each launcher
 function newThemeRow(settings, group) {
     let themesDir = Gio.File.new_for_path(Me.path + '/launcher');
     let themes = [];
+
+    // Check whats there
     try {
         let themeDirEnumerator = themesDir.enumerate_children
         ('standard::name', Gio.FileQueryInfoFlags.NONE, null);
@@ -32,35 +22,47 @@ function newThemeRow(settings, group) {
     } catch(e) {
         themes = [];
     }
+
+    // Uh compare shit
     themes.sort((a, b) => a.localeCompare(b));
     let options = themes.slice();
+
+    // Create an item
     options.unshift(_('Follow System'));
     const themeRow = new Adw.ComboRow({
         title: _('Theme'),
         model: Gtk.StringList.new(options),
     });
+
+    // Add new row/item
     group.add(themeRow);
     let index = themes.indexOf(settings.get_string('Launcher'));
     themeRow.set_selected(index + 1);
+
+    // Bind to settings
     themeRow.connect('notify::selected', () => {
         let selected = themeRow.get_selected();
         let theme = selected == 0 ? '' : themes[selected - 1];
         settings.set_string('Launcher', theme);
     });
-    settings.connect('changed::theme', () => {
+    settings.connect('changed::Launcher', () => {
         let index = themes.indexOf(settings.get_string('Launcher'));
         themeRow.set_selected(index + 1);
     });
 }
 
-export default class DynamicIconsPreferences extends ExtensionPreferences {
+
+
+// Create pref page
+export default class LauncherPref extends ExtensionPreferences {
     fillPreferencesWindow(window) {
-        Me = this;
-        const settings = Me.getSettings('org.gnome.shell.extensions.unityglass');
-        const page = new Adw.PreferencesPage();
+        Me                  = this;
+        const settings      = Me.getSettings('org.gnome.shell.extensions.unityglass');
+        const page          = new Adw.PreferencesPage();
         window.add(page);
-        
-        const themeGroup = new Adw.PreferencesGroup();
+
+        // Themegroup is launcher thees
+        const themeGroup    = new Adw.PreferencesGroup();
         page.add(themeGroup);
         newThemeRow(settings, themeGroup);
 
