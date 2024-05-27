638964af5544dd45b8fc68942e2b69bbea801483
teamcons_atwork <littlepastelworld@gmail.com>
2024-05-27 14:15:03 +0200

    r

--- unityglass@teamcons.carrd.co/prefs.js
+++ unityglass@teamcons.carrd.co/prefs.js
@@ -40,15 +40,15 @@ function newThemeRow(settings, group) {
         model: Gtk.StringList.new(options),
     });
     group.add(themeRow);
-    let index = themes.indexOf(settings.get_string('theme'));
+    let index = themes.indexOf(settings.get_string('Launcher'));
     themeRow.set_selected(index + 1);
     themeRow.connect('notify::selected', () => {
         let selected = themeRow.get_selected();
         let theme = selected == 0 ? '' : themes[selected - 1];
-        settings.set_string('theme', theme);
+        settings.set_string('Launcher', theme);
     });
     settings.connect('changed::theme', () => {
-        let index = themes.indexOf(settings.get_string('theme'));
+        let index = themes.indexOf(settings.get_string('Launcher'));
         themeRow.set_selected(index + 1);
     });
 }
@@ -56,42 +56,13 @@ function newThemeRow(settings, group) {
 export default class DynamicIconsPreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
         Me = this;
-        const settings = Me.getSettings
-        ('org.gnome.shell.extensions.unityglass');
+        const settings = Me.getSettings('org.gnome.shell.extensions.unityglass');
         const page = new Adw.PreferencesPage();
         window.add(page);
+        
         const themeGroup = new Adw.PreferencesGroup();
         page.add(themeGroup);
         newThemeRow(settings, themeGroup);
-        const calendarGroup = new Adw.PreferencesGroup({
-            title: _('Calendar'),
-        });
-        page.add(calendarGroup);
-        newRow(settings, calendarGroup, _('Dynamic Calendar Icon'), 'calendar');
-        newRow(settings, calendarGroup, _('Show Week Day'), 'show-weekday');
-        newRow(settings, calendarGroup, _('Show Month'), 'show-month');
-        const clocksGroup = new Adw.PreferencesGroup({
-            title: _('Clocks'),
-        });
-        page.add(clocksGroup);
-        newRow(settings, clocksGroup, _('Dynamic Clocks Icon'), 'clocks');
-        newRow(settings, clocksGroup, _('Show Seconds'), 'show-seconds');
-        const weatherGroup = new Adw.PreferencesGroup({
-            title: _('Weather'),
-        });
-        page.add(weatherGroup);
-        newRow(settings, weatherGroup, _('Dynamic Weather Icon'), 'weather');
-        newRow(settings, weatherGroup, _('Show Background'), 'show-background');
-        newRow(settings, weatherGroup, _('Show Temperature'), 'show-temperature');
-        settings.connect('changed::show-background', () => {
-            if(!settings.get_boolean('show-background')) {
-                settings.set_boolean('show-temperature', false);
-            }
-        });
-        settings.connect('changed::show-temperature', () => {
-            if(settings.get_boolean('show-temperature')) {
-                settings.set_boolean('show-background', true);
-            }
-        });
+
     }
 }
