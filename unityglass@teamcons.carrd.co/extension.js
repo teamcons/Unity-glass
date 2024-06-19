d6c878449b214ab36a390b0c5307631bd73bf56b
teamcons <littlepastelworld@gmail.com>
2024-06-19 22:39:52 +0200

    r

--- unityglass@teamcons.carrd.co/extension.js
+++ unityglass@teamcons.carrd.co/extension.js
@@ -3,40 +3,19 @@ import Gio from 'gi://Gio';
 import St from 'gi://St';
 
 
-// load the file
-// todo
-function loadTheme() {
-    let theme = settings.get_string('Launcher');
-    path = Me.path + '/launcher/' + theme;
-}
-
-
-
-
 
 // Main class
 //todo correct dispose and enable
 export default class ApplyUnityGlass
 {
-
-
-
     // Upon extension activation
     enable()
     {
-        Me = this;
-        settings = Me.getSettings('org.gnome.shell.extensions.unityglass');
-        loadTheme();
 
     }
-
-
-
-
     // Upon extension deactivation
     disable()
     {
-        Me = null;
-        destroyObjects();
+
     }
 }
